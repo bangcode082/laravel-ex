51a1ea811752eba3995f32a8c2c6fe41033fd434
Slamet Badwi <slamet.badwi@gmail.com>
2017-06-07 07:23:59 +0700

    Simple login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
